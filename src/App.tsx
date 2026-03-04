5d8aa6f33fbc6cdd846bfdbed54766c4fbfc8c00
rathishkumarlearning <rathishkumarlearning@gmail.com>
2026-03-04 08:32:01 -0500

    fix: HashRouter for GH Pages routing + lime green

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Reader from './pages/Reader'
 
 export default function App() {
   return (
-    <BrowserRouter basename="/tech-docs-v2">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/doc/:slug" element={<Reader />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
